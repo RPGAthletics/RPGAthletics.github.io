2fa7a32d90eff2b013ccf921e1b7a04385bbd94a
her_username <her_email>
2023-04-19 00:07:09 -0700

    update website

--- RpgWebsiteNew/RpgWebsite.gd
+++ RpgWebsiteNew/RpgWebsite.gd
@@ -34,39 +34,47 @@ func _ready():
 	button_tiktok = find_node("ButtonTikTok")
 
 
-
-
-	var __ = button_shop.connect("pressed", self, "button_shop_pressed")
-	__ = button_blog.connect("pressed", self, "button_blog_pressed")
-	__ = button_video.connect("pressed", self, "button_video_pressed")
-	__ = button_faq.connect("pressed", self, "button_faq_pressed")
-	__ = button_about.connect("pressed", self, "button_about_pressed")
-	__ = button_contact.connect("pressed", self, "button_contact_pressed")
-	__ = button_youtube.connect("pressed", self, "button_youtube_pressed")
+	button_shop.connect("pressed", self, "button_shop_pressed")
+	button_blog.connect("pressed", self, "button_blog_pressed")
+	button_video.connect("pressed", self, "button_video_pressed")
+	button_faq.connect("pressed", self, "button_faq_pressed")
+	button_about.connect("pressed", self, "button_about_pressed")
+	button_contact.connect("pressed", self, "button_contact_pressed")
+	button_youtube.connect("pressed", self, "button_youtube_pressed")
+	button_insta.connect("pressed", self, "button_insta_pressed")
+	button_tiktok.connect("pressed", self, "button_tiktok_pressed")
 
 
 
 func rpg_title_icon_pressed():
-	var __ = get_tree().change_scene("res://RpgWebsiteMainPage.tscn")
+	get_tree().change_scene("res://RpgWebsiteMainPage.tscn")
 
 func button_shop_pressed():
-	var __ = get_tree().change_scene("res://RpgWebsiteShop.tscn")
+	get_tree().change_scene("res://RpgWebsiteShop.tscn")
 
 func button_blog_pressed():
-	var __ = get_tree().change_scene("res://RpgWebsiteBlog.tscn")
+	get_tree().change_scene("res://RpgWebsiteBlog.tscn")
 
 func button_video_pressed():
-	var __ = get_tree().change_scene("res://RpgWebsiteVideo.tscn")
+	get_tree().change_scene("res://RpgWebsiteVideo.tscn")
 
 func button_faq_pressed():
-	var __ = get_tree().change_scene("res://RpgWebsiteFaq.tscn")
+	get_tree().change_scene("res://RpgWebsiteFaq.tscn")
 
 func button_about_pressed():
-	var __ = get_tree().change_scene("res://RpgWebsiteAbout.tscn")
+	get_tree().change_scene("res://RpgWebsiteAbout.tscn")
 	
 func button_contact_pressed():
-	var __ = get_tree().change_scene("res://RpgWebsiteContact.tscn")
+	get_tree().change_scene("res://RpgWebsiteContact.tscn")
 
 
 func button_youtube_pressed():
-	var __ = OS.shell_open("https://www.youtube.com/@RPGMovement")
+	OS.shell_open("https://www.youtube.com/@RPGMovement")
+
+
+func button_insta_pressed():
+	OS.shell_open("https://www.instagram.com/rpeeg/")
+
+
+func button_tiktok_pressed():
+	OS.shell_open("https://www.tiktok.com/@r.peeg")
